92c0333cc4e86ab9aa7ddb71205c334995408f16
Neous1 <Neous1@users.noreply.github.com>
2017-04-20 13:10:08 -0400

    Added a background to the canvas

---
+++ js/main.js
@@ -0,0 +1,13 @@
+PlayState = {};
+PlayState.preload = function (){
+    this.game.load.image("background", "images/background.png");
+};
+PlayState.create = function(){
+    this.game.add.image(0, 0, "background");
+};
+
+window.onload = function (){
+    let game = new Phaser.Game(960, 600, Phaser.Auto, "game");
+    game.state.add("play", PlayState);
+    game.state.start("play");
+};
